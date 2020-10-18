502bf5fcb35bcb2ad381b77975fd9733c978cdc0
Maxime <maxime.attala@edu.devinci.fr>
2020-10-18 15:21:00 +0200

    :tada: init project

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Capture The Flag</title>
+</head>
+
+<body>
+    <main>
+        <div class="chapter-menu">
+            <div>
+                
+            </div>
+            <div>
+            
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
