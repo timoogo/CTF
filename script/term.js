8b352fe95bdac5f7ea8d6f9041ed11de0c5c66a2
Vauthier <alexandre.vauthier@edu.devinci.fr>
2020-10-26 22:31:29 +0100

    :construction:rien du tout:construction:

--- script/term.js
+++ script/term.js
@@ -2,7 +2,7 @@ $(function() {
   
     // Set the command-line prompt to include the user's IP Address
     //$('.prompt').html('[' + codehelper_ip["IP"] + '@HTML5] # ');
-      $('.prompt').html('[user@HTML5] # ');
+      $('.prompt').html('C:\\CTF\\view>');
   
     // Initialize a new terminal object
     var term = new Terminal('#input-line .cmdline', '#term_container output');
