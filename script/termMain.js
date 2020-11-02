fbd699116d99c4a2cc09ff7b83d14d5716a9cee9
Vauthier <alexandre.vauthier@edu.devinci.fr>
2020-11-02 14:57:26 +0100

    :sparkles:Css du nouveau terminal:sparkles:

---
+++ script/termMain.js
@@ -0,0 +1,8 @@
+var jesaispas = document.getElementById('term');
+var daron = document.getElementById('term-container');
+
+daron.addEventListener("click", fonction);
+
+function fonction() {
+    jesaispas.focus();
+};
