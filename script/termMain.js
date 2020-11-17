d9571e88fa802ecf3907d698b96b6434ecb4c4f9
Alexandre <alexandre.vauthier@edu.devinci.fr>
2020-11-17 18:43:11 +0100

    :bug:Terminal drag:bug:

--- script/termMain.js
+++ script/termMain.js
@@ -19,7 +19,7 @@ window.onload = function () {
 
 //Focus the current line whenever clicking in the terminal
 terminal.addEventListener("click", fonction);
-//terminal.addEventListener("click", DragTerminal);
+//terminal.addEventListener("clicsk", DragTerminal);
 
 function fonction() {
     currentLine.focus();
@@ -84,13 +84,13 @@ function CheckUserInput() {
     $('#term-container').append(termAnswer);
     var textAnswer = $('<p class="term_text answers">unknown command</p>');
     // Check the user input and execute the corresponding command
-    switch (currentLine.value) {
+    switch (currentLine.value){
         case "help":
             textAnswer = $('<p class="term_text answers">available functions : help <br>return <br>link <br>reload</p>');
             break;
         case "clear":
             textAnswer = ""
-            $(".answers").remove();
+            $( ".answers" ).remove();
             break;
         case "return":
             textAnswer = $('<p class="term_text answers">This is the return function</p>');
@@ -100,11 +100,26 @@ function CheckUserInput() {
             break;
         case "reload":
             textAnswer = $('<p class="term_text answers">reloading the page...</p>');
-            setTimeout(function () {
+            setTimeout(function(){
                 document.location.reload();
-            }, 2000);
+            },2000);
             break;
     }
     termAnswer.append(textAnswer);
 }
 
+function DragTerminal(){
+    mult = 1.8;
+    terminal_j.draggable({
+        containment: "#main-container",
+        cursor:"grabbing",
+        // drag: function (event, ui) {
+        //     ui.position.top += (ui.offset.top - ui.originalPosition.top) * mult;
+        //     ui.position.left += (ui.offset.left - ui.originalPosition.left) * mult;
+        // }
+    }); 
+   
+}
+$("#term-container").resizable();
+
+DragTerminal();
