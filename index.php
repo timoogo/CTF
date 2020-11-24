1b8be274c6fe27382c207a6c046bd3aa42ea6227
Maxime <maxime.attala@edu.devinci.fr>
2020-11-24 08:58:39 +0100

    change index.php

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
     <script src="script/jquery.js"></script>
-    <script src="script/jquery-ui.min.js"></script>
 
     <title>Capture The Flag</title>
 </head>
@@ -33,93 +32,83 @@
     </header>
 
 
-
-    <main>
-        <div class="chapter-menu">
-            <div class="left-menu">
-                <h2>Liste des chapitres</h2>
-                <div class="chapter">
-                    <p class="chapter-name"><i class="fas fa-angle-right"></i>Chapitre 1</p>
-                    <div class="chapter-content">
-                        <a class="levels" href="levels/level_tuto.php">Tutoriel</a>
-                        <a class="levels" href="levels/level_1.php">Niveau 1</a>
+    <div class="main-container">
+        <h1> Bienvenue sur notre CTF !</h1>
+        <div class="title-container">L'invite de commande</div>
+        <div class="sections-container">
+            <section>
+                <article>
+                    <div class="chapter titre-accueil">
+                        <p class="chapter-name">
+                            <i class="fas fa-angle-right" style="transform: rotate(0deg);"></i>
+                            Chapitre 1
+                        </p>
+                        <div class="chapter-content accueil">
+                            <a href="">Niveau tutoriel</a>
+                            <a href="">Niveau 1</a>
+                            <a href="">Niveau 2</a>
+                            <a href="">Niveau 3</a>
+                        </div>
                     </div>
-                    <hr>
-                </div>
-                <div class="chapter">
-                    <p class="chapter-name"><i class="fas fa-angle-right"></i>Chapitre 2</p>
-                    <div class="chapter-content">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur voluptatem, dolorum blanditiis quasi magni ab unde. Pariatur illum, possimus omnis ad eius dolorem deserunt! Laboriosam ullam nihil voluptatum sint vero.
+                </article>
+            </section>
+            <section>
+                <article>
+                    <div class="chapter titre-accueil">
+                        <p class="chapter-name">
+                            <i class="fas fa-angle-right" style="transform: rotate(0deg);"></i>
+                            Chapitre 1
+                        </p>
+                        <div class="chapter-content accueil">
+                            <a href="">Niveau tutoriel</a>
+                            <a href="">Niveau 1</a>
+                            <a href="">Niveau 2</a>
+                            <a href="">Niveau 3</a>
+                        </div>
                     </div>
-                    <hr>
-                </div>
-                <div class="chapter">
-                    <p class="chapter-name"><i class="fas fa-angle-right"></i>Chapitre 3</p>
-                    <div class="chapter-content">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur voluptatem, dolorum blanditiis quasi magni ab unde. Pariatur illum, possimus omnis ad eius dolorem deserunt! Laboriosam ullam nihil voluptatum sint vero.
+                </article>
+            </section>
+            <section>
+                <article>
+                    <div class="chapter titre-accueil">
+                        <p class="chapter-name">
+                            <i class="fas fa-angle-right" style="transform: rotate(0deg);"></i>
+                            Chapitre 1
+                        </p>
+                        <div class="chapter-content accueil">
+                            <a href="">Niveau tutoriel</a>
+                            <a href="">Niveau 1</a>
+                            <a href="">Niveau 2</a>
+                            <a href="">Niveau 3</a>
+                        </div>
                     </div>
-                    <hr>
-                </div>
-                <div class="chapter">
-                    <p class="chapter-name"><i class="fas fa-angle-right"></i>Chapitre 4</p>
-                    <div class="chapter-content">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur voluptatem, dolorum blanditiis quasi magni ab unde. Pariatur illum, possimus omnis ad eius dolorem deserunt! Laboriosam ullam nihil voluptatum sint vero.
+                </article>
+            </section>
+            <section>
+                <article>
+                    <div class="chapter titre-accueil">
+                        <p class="chapter-name">
+                            <i class="fas fa-angle-right" style="transform: rotate(0deg);"></i>
+                            Chapitre 1
+                        </p>
+                        <div class="chapter-content accueil">
+                            <a href="">Niveau tutoriel</a>
+                            <a href="">Niveau 1</a>
+                            <a href="">Niveau 2</a>
+                            <a href="">Niveau 3</a>
+                        </div>
                     </div>
-                    <hr>
-                </div>
-                <div class="chapter">
-                    <p class="chapter-name"><i class="fas fa-angle-right"></i>Chapitre 5</p>
-                    <div class="chapter-content">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur voluptatem, dolorum blanditiis quasi magni ab unde. Pariatur illum, possimus omnis ad eius dolorem deserunt! Laboriosam ullam nihil voluptatum sint vero.
-                    </div>
-                    <hr>
-                </div>
-            </div>
-            <div class="right-menu inactive">
-                <div class="circle"></div>
-                <div class="circle"></div>
-                <div class="circle"></div>
-            </div>
+                </article>
+            </section>
         </div>
 
-        <div id="main-container">
-            <div id="term-container">
-                <div class="line answers" id="currentLine">
-                    <p class="term_text answers">C:\CTF\DOS></p>
-                    <input name="command" type="text" class="term answers" id="currentCmd">
-                </div>
-            </div>
-        </div>
-
+    </div>
 
     </main>
-    <footer style="height: 0px;">
-        <div id="toDoList">
-            <div id="upperTDL">
-                <h3>To-Do List</h3>
-            </div>
-            <div id="lowerTDL">
-                <ol>
-                    <li>
-                        <hr>
-                        <h4>Faire ceci :</h4>
-                        <pre>
-                            <code>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, culpa at fuga sunt dolorem odit deserunt illum obcaecati voluptatibus laborum, vel quis quos est, repellat amet facere numquam illo iure?</code>
-                        </pre>
-                    </li>
-                    <li>
-                        <hr>
-                        <h4>Faire cela :</h4>
-                        <pre>
-                            <code>bonjoir</code>
-                        </pre>
-                    </li>
-                </ol>
-            </div>
-        </div>
+    <footer>
+        <p> &copy CTF, 2020 - Tous droits réservés </p>
     </footer>
     <script src="script/main.js"></script>
-    <script src="script/termMain.js"></script>
 </body>
 
 </html>
