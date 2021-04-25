09df60301369f21363172cdc895cf7f9f23dd86a
Alexandre <alexandre.vauthier@edu.devinci.fr>
2021-04-25 20:00:59 +0200

    todolist

--- Pages/levels/level_2-tuto.php
+++ Pages/levels/level_2-tuto.php
@@ -2,10 +2,9 @@
 
 include "../../Components/init.php";
 include "../header.php";
-include "../../Components/left_menu.php";
 
 
-if(!empty($_POST)){
+if (!empty($_POST)) {
 
       // var_dump($_POST);
       // echo "<br>";
@@ -24,10 +23,11 @@ if(!empty($_POST)){
       // echo "<br>";
       // var_dump($query->rowCount());
 
-      if ($query->rowCount() >0){
+      if ($query->rowCount() > 0) {
             header('location:' . ROOT . '/Pages/levels/level_2-1.php');
       }
 }
+include "../../Components/left_menu.php";
 ?>
 
 <div id="main-container">
@@ -35,12 +35,12 @@ if(!empty($_POST)){
       <div class="levelDescription">
             <p data-tooltip-container>Pour ce deuxième chapitre, vous allez découvrir les failles de sécurité SQL</p>
             <p data-tooltip-container>Votre but pour ce premier niveau sera de réaliser une injection SQL, chose réalisable lorsque les valeurs des champs ne sont pas vérifiés et sécurisés. Essayez de passer au prochain niveau sans pour autant connaitre le nom d'utilisateur ou le mot de passe</p>
-            
+
       </div>
 
       <form method="POST">
 
-            <Label for="user" style="color: white;">Username</Label>  
+            <Label for="user" style="color: white;">Username</Label>
             <input id="user" type="text" name="user" placeholder="Username">
 
 
@@ -52,5 +52,28 @@ if(!empty($_POST)){
 
 </div>
 
+<div id="toDoList">
+      <div id="upperTDL">
+            <h3>To-Do List</h3>
+      </div>
+      <div id="lowerTDL">
+            <ol>
+                  <li>
+                        <hr>
+                        <h4>Requête SQL :</h4>
+                        <pre>
+                        <code>SELECT * <br>FROM administrators <br>WHERE admin_name = '$user' <br>AND admin_password = '$pass'</code>
+                    </pre>
+                  </li>
+                  <li>
+                        <hr>
+                        <h4>Faire cela :</h4>
+                        <pre>
+                        <code>Essayer une injection sql, par exemple en contournant les conditions avec un <span style="color: #FA2913;">OR</span> ou <span style="color: #FA2913;">//</span> ou un <span style="color: #FA2913;">#</span></code>
+                    </pre>
+                  </li>
+            </ol>
+      </div>
+</div>
 
 <script src="<?= ROOT ?>/script/main.js"></script>
--- Pages/levels/level_tuto.php
+++ Pages/levels/level_tuto.php
@@ -3,63 +3,63 @@ include "../../Components/init.php";
 include "../header.php";
 include "../../Components/left_menu.php";
 ?>
-    <main data-id="1">
-        <div id="main-container">
-            <div class="levelDescription">
-                <p data-tooltip-container data-tooltip-label="Try help command if you are lost">Bienvenue sur notre site de CTF ! Le premier niveau consiste à se familiariser avec le DOS.</p>
-                <p data-tooltip-container data-tooltip-label="Try help command if you are lost">Pour commencer vous pouvez écrire la commande help pour découvrir les différentes commandes. Votre but sera de trouver les flags pour pouvoir finir le niveau</p>
-                
-            </div>
-            <div id="term-container">
-                <div class="line answers" id="currentLine">
-                    <p class="term_text answers">C:\CTF\DOS></p>
-                    <input name="command" type="text" class="term answers" id="currentCmd">
-                </div>
-            </div>
-
-            <div class="dragNote">
-                <textarea wrap="hard" name="notepad" id="notepad" class="notepad" cols="30" rows="10" style="display: none;"></textarea>
-            </div>
-
-            <div class="notepad-btn">
-                <span style="font-size: 40px; color: #eeeeee;">
-                    <i class="far fa-sticky-note" ></i>
-                </span>
-            </div>
+<main data-id="1">
+    <div id="main-container">
+        <div class="levelDescription">
+            <p data-tooltip-container data-tooltip-label="Try help command if you are lost">Bienvenue sur notre site de CTF ! Le premier niveau consiste à se familiariser avec le DOS.</p>
+            <p data-tooltip-container data-tooltip-label="Try help command if you are lost">Pour commencer vous pouvez écrire la commande help pour découvrir les différentes commandes. Votre but sera de trouver les flags pour pouvoir finir le niveau</p>
 
         </div>
-    </main>
-
-
-    <div id="toDoList">
-        <div id="upperTDL">
-            <h3>To-Do List</h3>
-        </div>
-        <div id="lowerTDL">
-                <ol>
-                    <li>
-                        <hr>
-                        <h4>Faire ceci :</h4>
-                        <pre>
-                            <code>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, culpa at fuga sunt dolorem odit deserunt illum obcaecati voluptatibus laborum, vel quis quos est, repellat amet facere numquam illo iure?</code>
-                        </pre>
-                    </li>
-                    <li>
-                        <hr>
-                        <h4>Faire cela :</h4>
-                        <pre>
-                            <code>bonjoir</code>
-                        </pre>
-                    </li>
-                </ol>
+        <div id="term-container">
+            <div class="line answers" id="currentLine">
+                <p class="term_text answers">C:\CTF\DOS></p>
+                <input name="command" type="text" class="term answers" id="currentCmd">
             </div>
         </div>
-    <footer style="height: 0px;">
-    </footer>
 
-    <script src="<?= ROOT ?>/script/toolTip.js"></script>
-    <script src="<?= ROOT ?>/script/main.js"></script>
-    <script src="<?= ROOT ?>/script/termMain.js"></script>
+        <div class="dragNote">
+            <textarea wrap="hard" name="notepad" id="notepad" class="notepad" cols="30" rows="10" style="display: none;"></textarea>
+        </div>
+
+        <div class="notepad-btn">
+            <span style="font-size: 40px; color: #eeeeee;">
+                <i class="far fa-sticky-note"></i>
+            </span>
+        </div>
+
+    </div>
+</main>
+
+
+<div id="toDoList">
+    <div id="upperTDL">
+        <h3>To-Do List</h3>
+    </div>
+    <div id="lowerTDL">
+        <ol>
+            <li>
+                <hr>
+                <h4>Faire ceci :</h4>
+                <pre>
+                        <code>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, culpa at fuga sunt dolorem odit deserunt illum obcaecati voluptatibus laborum, vel quis quos est, repellat amet facere numquam illo iure?</code>
+                    </pre>
+            </li>
+            <li>
+                <hr>
+                <h4>Faire cela :</h4>
+                <pre>
+                        <code>bonjoir</code>
+                    </pre>
+            </li>
+        </ol>
+    </div>
+</div>
+<footer style="height: 0px;">
+</footer>
+
+<script src="<?= ROOT ?>/script/toolTip.js"></script>
+<script src="<?= ROOT ?>/script/main.js"></script>
+<script src="<?= ROOT ?>/script/termMain.js"></script>
 </body>
 
 </html>
